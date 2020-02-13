461b3eb2c927448f3fdc42d34fc76496a331a4fd
detrich <detrich2k@gmail.com>
2020-02-13 14:28:54 -0500

    finished nested

--- nested.py
+++ nested.py
@@ -1,22 +1,64 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "Detrich"
 
 import sys
+
+# (  )
+# [  ]
+# {  }
+# <  >
+# (*  *)
 
 
 def is_nested(line):
     """Validate a single input line for correct nesting"""
-    pass
+    matchingDict = {")": "(",
+                    ">": "<",
+                    "*)": "(*",
+                    "]": "[",
+                    "}": "{"}
+    holdparen = []
+    counter = 0
+    while line:
+        if line.startswith("(*"):
+            if "(*" in holdparen:
+                return counter
+            token = line[:2]
+        elif line.startswith("*)"):
+            token = line[:2]
+        else:
+            token = line[0]
+        counter += 1
+        if token in matchingDict.values():
+            holdparen.append(token)
+        if token in matchingDict.keys():
+            if matchingDict[token] == holdparen[-1]:
+                holdparen.pop()
+            else:
+                return counter
+        line = line[len(token):]
+    if holdparen:
+        return counter
+
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
+    with open('input.txt') as f:
+        with open('output.txt', 'w') as f_out:
+            for line in f:
+                result = is_nested(line)
+                print(result)
+                if result:
+                    f_out.write(f'No {result}\n')
+                else:
+                    f_out.write('Yes\n')
+    print(is_nested(sys.argv[1]))
 
 
 if __name__ == '__main__':
